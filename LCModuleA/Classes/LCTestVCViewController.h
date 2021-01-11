637d9ecc5be71c70c4872916cedb4a73979944cd
dudehan <13295142617@163.com>
2021-01-11 17:03:27 +0800

    no message

---
+++ LCModuleA/Classes/LCTestVCViewController.h
@@ -0,0 +1,17 @@
+//
+//  LCTestVCViewController.h
+//  MPFrameworkDemo_pod
+//
+//  Created by Dehan Du on 2021/1/11.
+//  Copyright © 2021 yangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCTestVCViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
